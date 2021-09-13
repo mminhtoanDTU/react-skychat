f414cffb5f95722828b3219e2f02a91e5e408c75
Lê Minh Toàn <mmminhle771@gmail.com>
2021-09-13 20:10:31 +0700

    v1.0.1

--- src/features/ChatApp/components/MainContent/InputMessage/index.jsx
+++ src/features/ChatApp/components/MainContent/InputMessage/index.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { IoHappy, IoPaperPlane } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
 import { addDocument } from '../../../../../firebase/service';
+import { EmojiButton } from '@joeattardi/emoji-button';
 import './inputmessage.scss';
 
 function InputMessage(props) {
     const [message, setMessage] = useState('');
     const { userInfo } = useSelector(state => state.user);
     const { selectedRoom } = useSelector(state => state.rooms);
+
+    const inputRef = useRef(null);
+
+    const picker = new EmojiButton({
+        position: {
+            right: '20px',
+            bottom: '60px'
+        },
+        autoHide: false,
+        showPreview: false,
+        showSearch: false,
+        showVariants: false
+    });
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
@@ -21,9 +35,21 @@ function InputMessage(props) {
         }
         setMessage('');
     }
+
+    const handleOpenEmoji = () => {
+        console.log(inputRef.current);
+        picker.on('emoji', selection => {
+            // handle the selected emoji here
+            const newMessage = message.concat(selection.emoji)
+            setMessage(newMessage)
+        });
+        picker.togglePicker(inputRef.current)
+    }
+
     return (
         <form className="box-input" onSubmit={(e) => handleOnSubmit(e)}>
             <input
+                ref={inputRef}
                 type="text"
                 className="input-message"
                 placeholder="Type a message"
@@ -36,8 +62,13 @@ function InputMessage(props) {
             >
                 <IoPaperPlane size="23px" />
             </div>
-            <div className="icon-smile">
-                <IoHappy size="22px" />
+            <div
+                className="icon-smile"
+                onClick={() => handleOpenEmoji()}
+            >
+                <IoHappy
+                    size="22px"
+                />
             </div>
         </form>
     );
--- src/firebase/configFirebase.js
+++ src/firebase/configFirebase.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import 'firebase/analytics';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
@@ -14,7 +13,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+//firebase.analytics();
 
 const auth = firebase.auth();
 const db = firebase.firestore();
